8d7bfce895a8168a4e9e11e0bfb51991f209003b
Demarius Chrite <chrite.demarius@gmail.com>
2013-08-06 15:42:37 -0400

    Map 1D variable to 2D coordinates.
    Converts distance to an (x, y) pair.

--- index.js
+++ index.js
@@ -10,9 +10,13 @@ function convertPointToDistance (height, x, y) {
     // For each Hilbert level, we want to add an amount to
     // `d` based on which region we are in
     for (level = height / 2; level > 0; level /= 2) {
+        // Determine what region we're in
         xbit = (x & level) > 0
         ybit = (y & level) > 0
+        // increase distance based on region
         d += level * level * ((3 * xbit) ^ ybit)
+        // rotate so that we'll be in sync with the next
+        // region.
         var temp = rotate (height, x, y, xbit, ybit)
         x = temp[0]
         y = temp[1]
@@ -21,6 +25,26 @@ function convertPointToDistance (height, x, y) {
     return d
 }
 
+function convertDistanceToPoint (height, distance) {
+    var xbit, ybit, level, distance
+    var x = 0, y = 0
+
+    for (level = 1; level < height; level *= 2) {
+        xbit = 1 & (distance / 2)
+        ybit = 1 & (distance ^ xbit)
+
+        var temp = rotate(height, x, y, xbit, ybit)
+        x = temp[0]
+        y = temp[1]
+        x += level * xbit
+        y += level * xbit
+        distance /= 4
+    }
+
+    return [x, y]
+}
+
+// Rotate the coordinate plane and (x,y)
 function rotate (n, x, y, xbit, ybit) {
     if (ybit === 0  ) {
         if (xbit === 1) {
