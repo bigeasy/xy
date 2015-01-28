cf605e80149b438ab3944ce7055b7f84c0ad9728
Demarius Chrite <chrite.demarius@gmail.com>
2015-01-28 19:10:23 -0400

    Fixing test.

--- t/xy/2dto1d.t.js
+++ t/xy/2dto1d.t.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 
 require('proof')(1, function (equal) {
-  var hilbert = require('../..')
+    var hilbert = require('../..')
 
-  equal(hilbert.xy2d(4, 0, 2), 16)
+    //equal(hilbert.xy2d(4, 0, 2), 16)
+    equal(hilbert.xy2d(16, 2, 2), 8)
 })
