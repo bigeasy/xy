41ce50b70ed0d7a510787f1015ffd79236854f98
Joshua Pelletier <jgpelletier@gmail.com>
2015-01-28 18:49:27 -0800

    Add functions for debugging.

--- t/xy/1dto3d.t.js
+++ t/xy/1dto3d.t.js
@@ -4,6 +4,7 @@ require('proof')(1, function (equal) {
   var hilbert = require('../..')
 
   equal(hilbert.xyz2d(16, 2, 2), 8)
+  /*
   console.log(hilbert.xyz2d(0,0,0))
   console.log(hilbert.xyz2d(0,1,0))
   console.log(hilbert.xyz2d(0,1,1))
@@ -12,4 +13,13 @@ require('proof')(1, function (equal) {
   console.log(hilbert.xyz2d(1,1,1))
   console.log(hilbert.xyz2d(1,1,0))
   console.log(hilbert.xyz2d(1,0,0))
+  */
+  hilbert.xyz2d(0,0,0)
+  hilbert.xyz2d(0,1,0))
+  hilbert.xyz2d(0,1,1))
+  hilbert.xyz2d(0,0,1))
+  hilbert.xyz2d(1,0,1))
+  hilbert.xyz2d(1,1,1))
+  hilbert.xyz2d(1,1,0))
+  hilbert.xyz2d(1,0,0))
 })
