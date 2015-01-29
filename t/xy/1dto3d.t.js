0dadde4c5b545ba7c1f49ec0479e6d257fba9054
Joshua Pelletier <jgpelletier@gmail.com>
2015-01-28 18:37:41 -0800

    Test length for n = 2.

--- t/xy/1dto3d.t.js
+++ t/xy/1dto3d.t.js
@@ -4,4 +4,12 @@ require('proof')(1, function (equal) {
   var hilbert = require('../..')
 
   equal(hilbert.xyz2d(16, 2, 2), 8)
+  console.log(hilbert.xyz2d(0,0,0))
+  console.log(hilbert.xyz2d(0,1,0))
+  console.log(hilbert.xyz2d(0,1,1))
+  console.log(hilbert.xyz2d(0,0,1))
+  console.log(hilbert.xyz2d(1,0,1))
+  console.log(hilbert.xyz2d(1,1,1))
+  console.log(hilbert.xyz2d(1,1,0))
+  console.log(hilbert.xyz2d(1,0,0))
 })
