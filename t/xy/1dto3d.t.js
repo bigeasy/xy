963ad5bab0f28cda1cce9a177c8a254540f68a04
Joshua Pelletier <jgpelletier@gmail.com>
2015-02-05 14:01:57 -0800

    Add distance and points to test.

--- t/xy/1dto3d.t.js
+++ t/xy/1dto3d.t.js
@@ -2,8 +2,21 @@
 
 require('proof')(1, function (assert) {
     var hilbert = require('../..')
+    /*
+    // vv actul tests
+    assert(hilbert.d2xyz(7, 2), [1, 0, 0])
+    assert(hilbert.d2xyz(63, 4), [3, 0, 0])
+    assert(hilbert.d2xyz(64, 8), [0, 0, 4])
+    assert(hilbert.d2xyz(65, 8), [0, 0, 5])
+    assert(hilbert.d2xyz(127, 8), [0, 3, 4])
+    assert(hilbert.d2xyz(192, 8), [0, 7, 3])
+    assert(hilbert.d2xyz(320, 8), [7, 7, 4])
+    assert(hilbert.d2xyz(511, 8), [7, 0, 0])
+    */
 
-    assert(hilbert.d2xyz(8, 16), [16, 2, 2])
+
+    // vvv will be erased. used to tinker with
+    // d2xyz function.
     console.log(hilbert.d2xyz(0, 2))
     console.log(hilbert.d2xyz(1, 2)) // 0,  0, 1
     console.log(hilbert.d2xyz(2, 2)) // 1,  0, 1
