a164f0b24593c6732621a23a775b93d928e01240
Demarius Chrite <chrite.demarius@gmail.com>
2015-02-06 18:25:09 -0400

    Expanding test for 2d <-> 1d.

--- t/xy/2dto1d.t.js
+++ t/xy/2dto1d.t.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 
-require('proof')(1, function (assert) {
+require('proof')(3, function (assert) {
     var hilbert = require('../..')
 
     //equal(hilbert.xy2d(4, 0, 2), 16)
     assert(hilbert.xy2d(16, 2, 2), 8)
+    assert(hilbert.xy2d(0, 0, 2), 0)
+    assert(hilbert.xy2d(0, 1, 3), 1)
 })
