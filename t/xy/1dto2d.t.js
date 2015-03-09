36a64d97db841e0218a832eebb00251e6ebb8671
Joshua Pelletier <jgpelletier@gmail.com>
2015-03-09 14:54:55 -0700

    Add test for 2d hilbert index.

--- t/xy/1dto2d.t.js
+++ t/xy/1dto2d.t.js
@@ -1,9 +1,19 @@
 #!/usr/bin/env node
 
-require('proof')(9, prove)
+require('proof')(18, prove)
 
 function prove (assert) {
     var hilbert = require('../..')
+
+    assert(hilbert.hilbertInverse(2,0), [0,0], "index 0 equals point [0,0]")
+    assert(hilbert.hilbertInverse(2,1), [1,0], "index 1 equals point [1,0]")
+    assert(hilbert.hilbertInverse(2,2), [1,1], "index 2 equals point [1,1]")
+    assert(hilbert.hilbertInverse(2,3), [1,0], "index 3 equals point [1,0]")
+    assert(hilbert.hilbertInverse(2,4), [0,2], "index 4 equals point [0,2]")
+    assert(hilbert.hilbertInverse(2,5), [0,3], "index 5 equals point [0,3]")
+    assert(hilbert.hilbertInverse(2,6), [1,3], "index 6 equals point [1,3]")
+    assert(hilbert.hilbertInverse(2,7), [1,2], "index 7 equals point [1,2]")
+    assert(hilbert.hilbertInverse(2,8), [2,2], "index 8 equals point [2,2]")
 
     assert(hilbert.hilbertInverse(3,0), [0,0,0], "index 0 equals point [0,0,0]")
     assert(hilbert.hilbertInverse(3,1), [0,1,0], "index 1 equals [0,1,0]")
